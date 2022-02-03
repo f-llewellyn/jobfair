4fef2839e88ba6332d7271bb9aa0cca6dd52a3f0
Finn Llewellyn <llewellynfinn@gmail.com>
2022-02-03 21:05:21 +0000

    Fixed centering elements

--- client/src/components/ListingContainer.vue
+++ client/src/components/ListingContainer.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="listings" :class="{center: this.filteredListings.length === 0}">
 		<!-- <p>{{ this.data }}</p> -->
-		<p v-if="this.filteredListings.length === 0" class="err">Sorry, there don't seem to be any jobs at the moment <span>😕</span> </p>
+		<p v-if="this.filteredListings.length === 0" class="err">
+			Sorry, there don't seem to be any jobs at the moment <span>😕</span>
+		</p>
 		<Listing
 			v-else
 			v-for="listing in this.filteredListings"
@@ -10,97 +12,102 @@
 			:pay="listing.pay.toFixed(2)"
 			:hours="listing.hours"
 			:location="listing.location"
-            :employer="listing.company"
-            :applicationUrl="listing.applicationUrl"
+			:employer="listing.company"
+			:applicationUrl="listing.applicationUrl"
 		/>
 	</div>
 </template>
 
 <script>
-import Listing from "./Listing.vue";
+	import Listing from "./Listing.vue";
 
-export default {
-	name: "listing-container",
-	components: {
-		Listing
-	},
-	props: ["filters"],
-	data() {
-		const data = [];
-
-		return {data}
-	},
-	methods: {
-	},
-	async mounted() {
-		try {
-			const response = await fetch("http://localhost:3000/api");
-			if (!response.ok) {
-				// get error message from body or default to response statusText
-				const error = (this.data && this.data.message) || response.statusText;
-				return Promise.reject(error);
-			}
-			// if ((await response.text()).length === 0) {
-			//     console.log("There's fuck all here")
-			// }
-			this.data = await response.json();
-		} catch (error) {
-			console.warn(error.message);
-		}
-	},
-	computed: {
-		getFilters() {
-			return this.filters
+	export default {
+		name: "listing-container",
+		components: {
+			Listing,
 		},
-		
-		filteredListings() {
-			return this.data
-			.filter(listing => {
-				if (this.getFilters.title) {
-					return listing.title.toLowerCase().includes(this.getFilters.title.toLowerCase())
-				} else {
-					return listing
+		props: ["filters"],
+		data() {
+			const data = [];
+
+			return {data};
+		},
+		methods: {},
+		async mounted() {
+			try {
+				const response = await fetch("http://localhost:3000/api");
+				if (!response.ok) {
+					// get error message from body or default to response statusText
+					const error =
+						(this.data && this.data.message) || response.statusText;
+					return Promise.reject(error);
 				}
-			})
-			.filter(listing => {
-				return listing.pay >= this.getFilters.pay
-			})
-			.filter(listing => {
-				if (this.getFilters.hours > 0) {
-					return listing.hours <= this.getFilters.hours
-				} else {
-					return listing
-				}
-			})
-			.filter(listing => {
-				if (this.getFilters.location) {
-					return listing.location.toLowerCase().includes(this.getFilters.location.toLowerCase())
-				} else {
-					return listing
-				}
-			})
-		}
-	},
-	
-};
+				// if ((await response.text()).length === 0) {
+				//     console.log("There's fuck all here")
+				// }
+				this.data = await response.json();
+			} catch (error) {
+				console.warn(error.message);
+			}
+		},
+		computed: {
+			getFilters() {
+				return this.filters;
+			},
+
+			filteredListings() {
+				return this.data
+					.filter((listing) => {
+						if (this.getFilters.title) {
+							return listing.title
+								.toLowerCase()
+								.includes(this.getFilters.title.toLowerCase());
+						} else {
+							return listing;
+						}
+					})
+					.filter((listing) => {
+						return listing.pay >= this.getFilters.pay;
+					})
+					.filter((listing) => {
+						if (this.getFilters.hours > 0) {
+							return listing.hours <= this.getFilters.hours;
+						} else {
+							return listing;
+						}
+					})
+					.filter((listing) => {
+						if (this.getFilters.location) {
+							return listing.location
+								.toLowerCase()
+								.includes(
+									this.getFilters.location.toLowerCase()
+								);
+						} else {
+							return listing;
+						}
+					});
+			},
+		},
+	};
 </script>
 
 <style scoped>
-    .listings {
-        display: flex;
-        gap: 1.5rem;
-        flex-direction: column;
-        align-items: center;
-        margin: 1rem;
-        margin-bottom: 4rem;
-    }
+	.listings {
+		display: flex;
+		gap: 1.5rem;
+		flex-direction: column;
+		align-items: center;
+		/* margin: 1rem; */
+		margin-bottom: 4rem;
+	}
 
-    .err {
-        color: var(--grey);
-        max-width: 27ch;
-    }
+	.err {
+		color: var(--grey);
+		max-width: 27ch;
+	}
 
-    span {
-        font-weight: normal;
-    }
+	span {
+		font-weight: normal;
+	}
 </style>
--- client/src/views/Discover.vue
+++ client/src/views/Discover.vue
@@ -1,44 +1,39 @@
 <template>
-    <Header />
-    <div class="center">
-        <Search @receiveFilters="checkFilters($event)"/>
-        <ListingContainer :filters="this.filters"/>
-    </div>
+	<Header />
+	<div class="container">
+		<Search @receiveFilters="checkFilters($event)" />
+		<ListingContainer :filters="this.filters" />
+	</div>
 </template>
 
 <script>
-import { ref } from "vue"
-import Header from "../components/Header.vue"
-import Search from "../components/Search.vue"
-import ListingContainer from "../components/ListingContainer.vue"
+	import {ref} from "vue";
+	import Header from "../components/Header.vue";
+	import Search from "../components/Search.vue";
+	import ListingContainer from "../components/ListingContainer.vue";
 
-export default {
-    name: "discover",
-    components: {
-        Header,
-        Search,
-        ListingContainer
-    },
-    data() {
-        let filters = ref({})
+	export default {
+		name: "discover",
+		components: {
+			Header,
+			Search,
+			ListingContainer,
+		},
+		data() {
+			let filters = ref({});
 
-        return {filters}
-    },
-    methods: {
-        checkFilters(data) {
-            this.filters = data
-        }
-    }
-}
-
+			return {filters};
+		},
+		methods: {
+			checkFilters(data) {
+				this.filters = data;
+			},
+		},
+	};
 </script>
 
 <style scoped>
-    .center {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
+	.container {
+		padding: 0 1rem;
+	}
 </style>
-
