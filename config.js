23c0badb17eda7fecafebba4631334de055000ae
Finn Llewellyn <llewellynfinn@gmail.com>
2022-03-28 16:02:10 +0100

    Properly set up env vars

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
-    mongoUri: "mongodb+srv://finnl1234:09oeBsJcSutj9uzc@mevn.hbxeb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    PORT: process.env.PORT || 3000
-}
+	mongoUri: process.env.MONGO_URI,
+	PORT: process.env.PORT || 3000,
+};
